74b27db7ec9e4d0e507f4736cea536d73906929c
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 01:08:45 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -1,5 +1,14 @@
 import streamlit as st
-from util import set_background
+
+def set_background_color(hex_color="#F0F0F0"):
+    style = f"""
+        <style>
+        [data-testid="stAppViewContainer"] {{
+            background-color: {hex_color};
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
 
 set_background("#12B9C8")
 st.title(":green[FACT CHECK]")
