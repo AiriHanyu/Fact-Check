9bd767d4a213b889935cd3e1393bddbca849fca9
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 04:30:58 +0800

    Update util.py

--- util.py
+++ util.py
@@ -13,26 +13,15 @@ def set_background_color(hex_color="#F0F0F0"):
     """
     st.markdown(style, unsafe_allow_html=True)
 
-
-
-
 def get_text_from_url(url):
     try:
-        # Mengambil konten dari URL
         response = requests.get(url)
-        response.raise_for_status()  # Untuk memastikan bahwa request berhasil
-        
-        # Menggunakan BeautifulSoup untuk parse HTML
+        response.raise_for_status() 
         soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Mengambil semua teks dalam tag <p>
         paragraphs = soup.find_all('p')
         text = "\n".join([para.get_text() for para in paragraphs])
-        
         return text
     except requests.exceptions.RequestException:
-        # Jika ada masalah dengan request (misalnya URL tidak bisa diakses)
         return "Link tidak dapat diakses. Pastikan URL valid dan dapat dijangkau."
     except Exception:
-        # Untuk kesalahan lainnya
         return "Akses ke URL ditolak atau kesalahan lainnya."
