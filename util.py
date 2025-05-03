d9866dbd48c5eca8e8a5b3799348f8db09dde4c6
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 04:29:06 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import base64
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
 def set_background_color(hex_color="#F0F0F0"):
     style = f"""
@@ -10,3 +12,27 @@ def set_background_color(hex_color="#F0F0F0"):
         </style>
     """
     st.markdown(style, unsafe_allow_html=True)
+
+
+
+
+def get_text_from_url(url):
+    try:
+        # Mengambil konten dari URL
+        response = requests.get(url)
+        response.raise_for_status()  # Untuk memastikan bahwa request berhasil
+        
+        # Menggunakan BeautifulSoup untuk parse HTML
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Mengambil semua teks dalam tag <p>
+        paragraphs = soup.find_all('p')
+        text = "\n".join([para.get_text() for para in paragraphs])
+        
+        return text
+    except requests.exceptions.RequestException:
+        # Jika ada masalah dengan request (misalnya URL tidak bisa diakses)
+        return "Link tidak dapat diakses. Pastikan URL valid dan dapat dijangkau."
+    except Exception:
+        # Untuk kesalahan lainnya
+        return "Akses ke URL ditolak atau kesalahan lainnya."
