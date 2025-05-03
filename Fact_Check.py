1f3baf5b4db91fbaa509df4147fc53681b7452bb
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 01:15:05 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -2,6 +2,6 @@ import streamlit as st
 from util import set_background_color
 
 set_background_color("#12B9C8")
-st.title(":black[FACT CHECK]")
+st.markdown('<h1 style="color:black; font-size: 50px;">FACT CHECK</h1>', unsafe_allow_html=True)
 
 
