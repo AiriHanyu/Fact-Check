2833a2ae1016833bd5b37e05a2bc3733a7275e3f
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 01:50:29 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -3,10 +3,11 @@ from util import set_background_color
 
 set_background_color("#12B9C8")
 
-st.markdown("""
-    <div style="text-align: center;">
-        <h1 style="color:black; font-size: 100px; margin-bottom: 0px; line-height: 1.1;">FACT CHECK</h1>
-        <h4 style="color: black; margin-top: 0px; line-height: 1;">Not sure what to believe? Drop it here and let's check the facts and verify it here!</h4>
-    </div>
-""", unsafe_allow_html=True)
+st.markdown('<h1 style="color:black; font-size: 100px; text-align: center; margin-bottom: 0;">FACT CHECK</h1>', unsafe_allow_html=True)
+st.markdown(
+    "<h4 style='text-align: center; color: black; font-family: \"Times New Roman\", serif;'>Not sure what to believe? Drop it here and let's check the facts and verify it here!</h4>",
+    unsafe_allow_html=True
+)
+
+
 
