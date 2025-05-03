b54fb219eb2cf4ed1e064ef37e3334d8ae36c8c3
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 01:51:28 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -5,9 +5,15 @@ set_background_color("#12B9C8")
 
 st.markdown('<h1 style="color:black; font-size: 100px; text-align: center; margin-bottom: 0;">FACT CHECK</h1>', unsafe_allow_html=True)
 st.markdown(
-    "<h4 style='text-align: center; color: black; font-family: \"Times New Roman\", serif;'>Not sure what to believe? Drop it here and let's check the facts and verify it here!</h4>",
+    """
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    </head>
+    <h4 style='text-align: center; color: black; font-family: "Roboto", sans-serif;'>
+        Not sure what to believe? Drop it here and let's check the facts and verify it here!
+    </h4>
+    """,
     unsafe_allow_html=True
 )
-
 
 
