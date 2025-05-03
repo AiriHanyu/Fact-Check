e792bec36f38b4a9a7b54b70c2e0cbc7bc22f66e
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 00:32:29 +0800

    Create 1_About_Us.py

---
+++ pages/1_About_Us.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("About Us")
+
+st.markdown("""
+### AIMAR ANSHARI  
+[Instagram](https://instagram.com/aynshz_ryuxzy)
+
+---
+
+### DEWI AURA SYALSABILLAH F  
+[Instagram](https://instagram.com/quivyy_)
+""")
