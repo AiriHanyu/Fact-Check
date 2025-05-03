120f8a18b7392ac2ad6fcf7fd75422f4088f3878
Aimar Anshari <aiimaransharii@gmail.com>
2025-05-04 01:40:17 +0800

    Update 1_About_Us.py

--- pages/1_About_Us.py
+++ pages/1_About_Us.py
@@ -4,7 +4,11 @@ from util import set_background_color
 set_background_color("#12B9C8")
 
 st.markdown('<h1 style="color:black; font-size: 100px; text-align: center;">ABOUT US</h1>', unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: gray;'>Not sure what to believe? Drop it here and let’s check the facts and verify it here!</h4>", unsafe_allow_html=True)
+st.markdown(
+    "<h4 style='text-align: center; color: gray;'>Not sure what to believe? Drop it here and let’s check the facts and verify it here!</h4>",
+    unsafe_allow_html=True
+)
+
 
 st.markdown("""
     <div style="color: black;">
