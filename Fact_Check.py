4a5dba20a197ff5d969a925458d50293a83de4b0
Aimar Anshari <aiimaransharii@gmail.com>
2025-07-09 17:51:08 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -65,61 +65,61 @@ if st.session_state.tab:
     with col3:
         view = st.button("View")
 
-        if view:
-            with st.container():
-                if st.session_state.tab == "TEXT" and user_text:
-                    st.markdown(f"""
-                        <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
-                                    background-color: #ffffff; color: black;
-                                    max-width: 100%; overflow-wrap: break-word;
-                                    word-wrap: break-word; text-align: justify;">
-                            {user_text}
-                        </div>
-                    """, unsafe_allow_html=True)
+    if view:
+        with st.container():
+            if st.session_state.tab == "TEXT" and user_text:
+                st.markdown(f"""
+                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
+                                background-color: #ffffff; color: black;
+                                max-width: 100%; overflow-wrap: break-word;
+                                word-wrap: break-word; text-align: justify;">
+                        {user_text}
+                    </div>
+                """, unsafe_allow_html=True)
     
-                elif st.session_state.tab == "URL" and user_url:
-                    article_text = get_text_from_url(user_url)
-                    st.markdown(f"""
-                        <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
-                                    background-color: #ffffff; color: black;
-                                    max-width: 100%; overflow-wrap: break-word;
-                                    word-wrap: break-word; text-align: justify;">
-                            {article_text.replace('\n','')}
-                        </div>
-                    """, unsafe_allow_html=True)
+            elif st.session_state.tab == "URL" and user_url:
+                article_text = get_text_from_url(user_url)
+                st.markdown(f"""
+                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
+                                background-color: #ffffff; color: black;
+                                max-width: 100%; overflow-wrap: break-word;
+                                word-wrap: break-word; text-align: justify;">
+                        {article_text.replace('\n','')}
+                    </div>
+                """, unsafe_allow_html=True)
                 
-                elif st.session_state.tab == "DOC" and uploaded_file:
-                    doc_text = read_uploaded_file(uploaded_file)
-                    st.markdown(f"""
-                        <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
-                                    background-color: #ffffff; color: black;
-                                    max-width: 100%; overflow-wrap: break-word;
-                                    word-wrap: break-word; text-align: justify;">
-                            {doc_text.replace('\n','')}
-                        </div>
-                    """, unsafe_allow_html=True)
+            elif st.session_state.tab == "DOC" and uploaded_file:
+                doc_text = read_uploaded_file(uploaded_file)
+                st.markdown(f"""
+                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
+                                background-color: #ffffff; color: black;
+                                max-width: 100%; overflow-wrap: break-word;
+                                word-wrap: break-word; text-align: justify;">
+                        {doc_text.replace('\n','')}
+                    </div>
+                """, unsafe_allow_html=True)
 
-                elif st.session_state.tab == "MP3" and uploaded_file:
-                    doc_text = transcribe_audio(uploaded_file)
-                    st.markdown(f"""
-                        <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
-                                    background-color: #ffffff; color: black;
-                                    max-width: 100%; overflow-wrap: break-word;
-                                    word-wrap: break-word; text-align: justify;">
-                            {doc_text.replace('\n','')}
-                        </div>
-                    """, unsafe_allow_html=True)
+            elif st.session_state.tab == "MP3" and uploaded_file:
+                doc_text = transcribe_audio(uploaded_file)
+                st.markdown(f"""
+                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
+                                background-color: #ffffff; color: black;
+                                max-width: 100%; overflow-wrap: break-word;
+                                word-wrap: break-word; text-align: justify;">
+                        {doc_text.replace('\n','')}
+                    </div>
+                """, unsafe_allow_html=True)
 
-                elif st.session_state.tab == "MP4" and uploaded_file:
-                    doc_text = transcribe_video_to_text(uploaded_file)
-                    st.markdown(f"""
-                        <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
-                                    background-color: #ffffff; color: black;
-                                    max-width: 100%; overflow-wrap: break-word;
-                                    word-wrap: break-word; text-align: justify;">
-                            {doc_text.replace('\n','')}
-                        </div>
-                    """, unsafe_allow_html=True)
+            elif st.session_state.tab == "MP4" and uploaded_file:
+                doc_text = transcribe_video_to_text(uploaded_file)
+                st.markdown(f"""
+                    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 5px;
+                                background-color: #ffffff; color: black;
+                                max-width: 100%; overflow-wrap: break-word;
+                                word-wrap: break-word; text-align: justify;">
+                        {doc_text.replace('\n','')}
+                    </div>
+                """, unsafe_allow_html=True)
             
             st.markdown("<br>", unsafe_allow_html=True)
 
