b57a814adb0baaee7244fe612841ed88aa8db42a
Aimar Anshari <aiimaransharii@gmail.com>
2025-07-10 16:55:04 +0800

    Update Fact_Check.py

--- Fact_Check.py
+++ Fact_Check.py
@@ -73,10 +73,9 @@ if st.session_state.tab:
             if st.session_state.tab == "TEXT" and user_text:
                 proba = classify(user_text)
                 proba = classify(user_text)[0]  
-                st.markdown('<h1 style="color:black; font-size: 50px; text-align: center; margin-bottom: 0;">HASIL PREDIKSI</h1>', unsafe_allow_html=True)
+                st.markdown('<h1 style="color:black; font-size: 48px; text-align: center; margin-bottom: 0;">HASIL PREDIKSI</h1>', unsafe_allow_html=True)
                 for i in range(len(labels)):
-                    st.markdown(f"<h4 style='color:{colors[i]}; font-size: 50px; text-align: center;'>{labels[i]}: {proba[i]*100:.2f}%</h4>", unsafe_allow_html=True)
-                    st.progress(proba[i])
+                    st.markdown(f"<h4 style='color:{colors[i]}; font-size: 24px; text-align: center;'>{labels[i]}: {proba[i]*100:.2f}%</h4>", unsafe_allow_html=True)
     
             elif st.session_state.tab == "URL" and user_url:
                 article_text = get_text_from_url(user_url)
