f7c65427c66c1dc8c42f1ae516862c56b304be32
Aimar Anshari <aiimaransharii@gmail.com>
2025-09-09 17:34:39 +0800

    Update 1_Verification.py

--- pages/1_Verification.py
+++ pages/1_Verification.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import pandas as pd
 from util import set_background_color
 
 set_background_color("#A9A9A9")
 
-st.markdown('<h1 style="color:black; font-size: 50px; text-align: center;">RIWAYAT VERIFIKASI</h1>', unsafe_allow_html=True)
+st.markdown(
+    '<h1 style="color:black; font-size: 50px; text-align: center;">RIWAYAT VERIFIKASI</h1>',
+    unsafe_allow_html=True
+)
+
+data = {
+    "Waktu": ["09/09/2025 16:30", "09/09/2025 17:00", "09/09/2025 17:15"],
+    "Narasi Berita": [
+        "Berita A dicek dan masuk tahap validasi.",
+        "Berita B terindikasi hoaks.",
+        "Berita C sedang dalam proses klarifikasi."
+    ],
+    "Aksi": ["Silahkan menunggu konfirmasi selanjutnya"] * 3
+}
+
+df = pd.DataFrame(data)
+
+st.dataframe(df, use_container_width=True)
