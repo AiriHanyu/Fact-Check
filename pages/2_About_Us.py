b66f06c619030e377a7e82a19e6cf39aef7ed40a
Aimar Anshari <aiimaransharii@gmail.com>
2025-09-09 17:29:04 +0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from util import set_background_color
 
-set_background_color("#12B9C8")
+set_background_color("#A9A9A9")
 
 st.markdown('<h1 style="color:black; font-size: 100px; text-align: center;">ABOUT US</h1>', unsafe_allow_html=True)
 
