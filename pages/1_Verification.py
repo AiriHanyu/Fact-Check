032286432d2a1a9ce08d86ab34299ae2e9a7fb37
Aimar Anshari <aiimaransharii@gmail.com>
2025-09-09 17:31:18 +0800

    Update 1_Verification.py

--- pages/1_Verification.py
+++ pages/1_Verification.py
@@ -3,4 +3,4 @@ from util import set_background_color
 
 set_background_color("#A9A9A9")
 
-st.markdown('<h1 style="color:black; font-size: 100px; text-align: center;">RIWAYAT VERIFIKASI</h1>', unsafe_allow_html=True)
+st.markdown('<h1 style="color:black; font-size: 50px; text-align: center;">RIWAYAT VERIFIKASI</h1>', unsafe_allow_html=True)
